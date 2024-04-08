cc2aef6a843d3cfeb7f40d02ccb56d07422c35cd
jimhunterangie Hurst  .̲͚͍̬̩͇͙̬̮̹͎͖̘̹͂̓̒̈́̾̈́̾͑́͋͋͋̓ͣ̀ <hangingwithhunter4@gmail.com>
2024-04-08 16:18:32 -0400

    created throughRoutes code

---
+++ routes/thoughtRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { getAllThoughts, createThought, deleteThought } = require('../controllers/thoughtController');
+
+router.get('/', getAllThoughts);
+router.post('/', createThought);
+router.delete('/:id', deleteThought);
+
+module.exports = router;
